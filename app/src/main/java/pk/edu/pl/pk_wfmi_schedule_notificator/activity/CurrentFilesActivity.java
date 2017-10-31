cdf0a8f0c9768fbe3c318320fa29a412767abfde
kamex737 <kamex737@gmail.com>
2017-10-31 18:57:27 +0100

    Added simple view

---
+++ app/src/main/java/pk/edu/pl/pk_wfmi_schedule_notificator/activity/CurrentFilesActivity.java
@@ -0,0 +1,15 @@
+package pk.edu.pl.pk_wfmi_schedule_notificator.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import pk.edu.pl.pk_wfmi_schedule_notificator.R;
+
+public class CurrentFilesActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_files);
+    }
+}
