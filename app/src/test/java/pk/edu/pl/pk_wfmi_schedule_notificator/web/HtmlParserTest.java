e8a805d22bcb310b37a3824d054faa9aabe729fc
Pawel Czarny <czarny.pawel@student.pk.edu.pl>
2017-11-01 14:23:03 +0100

    Html parser

--- app/src/test/java/pk/edu/pl/pk_wfmi_schedule_notificator/web/HtmlParserTest.java
+++ app/src/test/java/pk/edu/pl/pk_wfmi_schedule_notificator/web/HtmlParserTest.java
@@ -1,10 +1,37 @@
 package pk.edu.pl.pk_wfmi_schedule_notificator.web;
-
-import org.junit.Test;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 public class HtmlParserTest {
-    @Test
-    public void fetchXlsFiles() throws Exception {
-    }
+    String url = "http://www.fmi.pk.edu.pl/?page=rozklady_zajec.php&nc";
 
+
+
+
+
+
+
+    public void fetchXlsFiles() throws Exception {
+        String xls = null;
+        try{
+            Document doc = Jsoup.connect(url).get();
+            Elements links = doc.select("a[href]");
+            for(Element link : links)
+            {
+                String name=link.toString();
+                String name2;
+                if(name.indexOf("NIESTACJONARNE")!=-1)
+                {
+                    name2=name.substring(name.indexOf("\"")+1);
+                    xls = (name2.substring(0, name2.indexOf("xls")+3));
+                }
+                else
+                    xls=null;
+            }
+        }catch(IOException exception){
+            exception.printStackTrace();
+        }
+    }
 }
