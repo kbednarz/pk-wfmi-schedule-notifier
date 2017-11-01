726f98cd42e00fe05166633dceeb034a9476d127
kamex737 <kamex737@gmail.com>
2017-11-01 18:33:52 +0100

    HtmlParser body moved to proper class

--- app/src/main/java/pk/edu/pl/pk_wfmi_schedule_notificator/web/HtmlParser.java
+++ app/src/main/java/pk/edu/pl/pk_wfmi_schedule_notificator/web/HtmlParser.java
@@ -1,19 +1,34 @@
 package pk.edu.pl.pk_wfmi_schedule_notificator.web;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 
 public class HtmlParser {
+    private final static String url = "http://www.fmi.pk.edu.pl/?page=rozklady_zajec.php&nc";
+
     /**
      * Looks for XML files on given page
+     *
      * @param pageUrl page address to download and parse a html
      * @return list of found files
      */
-    public List<String> fetchXlsFiles(String pageUrl){
-        // use Jsoup library (https://jsoup.org)
-        // stub below
-        return new ArrayList<String>(Arrays.asList("plik1.xls","plik2.xls"));
+    public String fetchXlsFiles(String pageUrl) throws IOException {
+        Document doc = Jsoup.connect(url).get();
+        Elements links = doc.select("a[href]");
+        for (Element link : links) {
+            String name = link.toString();
+            String name2;
+            if (name.indexOf("NIESTACJONARNE") != -1) {
+                name2 = name.substring(name.indexOf("\"") + 1);
+                return (name2.substring(0, name2.indexOf("xls") + 3));
+            }
+
+        }
+        return null;
     }
 }
--- app/src/test/java/pk/edu/pl/pk_wfmi_schedule_notificator/web/HtmlParserTest.java
+++ app/src/test/java/pk/edu/pl/pk_wfmi_schedule_notificator/web/HtmlParserTest.java
@@ -1,37 +1,20 @@
 package pk.edu.pl.pk_wfmi_schedule_notificator.web;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Element;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class HtmlParserTest {
-    String url = "http://www.fmi.pk.edu.pl/?page=rozklady_zajec.php&nc";
+    @Test
+    public void fetchXlsFilesTest() throws IOException {
+        HtmlParser htmlParser = new HtmlParser();
 
+        String result = htmlParser.fetchXlsFiles("");
 
-
-
-
-
-
-    public void fetchXlsFiles() throws Exception {
-        String xls = null;
-        try{
-            Document doc = Jsoup.connect(url).get();
-            Elements links = doc.select("a[href]");
-            for(Element link : links)
-            {
-                String name=link.toString();
-                String name2;
-                if(name.indexOf("NIESTACJONARNE")!=-1)
-                {
-                    name2=name.substring(name.indexOf("\"")+1);
-                    xls = (name2.substring(0, name2.indexOf("xls")+3));
-                }
-                else
-                    xls=null;
-            }
-        }catch(IOException exception){
-            exception.printStackTrace();
-        }
+        assertNotNull(result);
+        assertEquals("Kopia NIESTACJONARNE_2017_10_19-2.xls", result);
     }
 }
