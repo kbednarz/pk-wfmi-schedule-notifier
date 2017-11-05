bcd51f28b3e30ae239c05bbc80aa43ff98b48668
kamex737 <kamex737@gmail.com>
2017-11-05 20:01:23 +0100

    Schedule is displayed on view

--- app/src/main/java/pk/edu/pl/pk_wfmi_schedule_notificator/activity/CurrentFilesActivity.java
+++ app/src/main/java/pk/edu/pl/pk_wfmi_schedule_notificator/activity/CurrentFilesActivity.java
@@ -1,15 +1,30 @@
 package pk.edu.pl.pk_wfmi_schedule_notificator.activity;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import pk.edu.pl.pk_wfmi_schedule_notificator.R;
+import pk.edu.pl.pk_wfmi_schedule_notificator.web.HtmlParser;
 
 public class CurrentFilesActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_files);
+
+        ListView filesView = findViewById(R.id.filesView);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.listview_row, new ArrayList<String>());
+        filesView.setAdapter(arrayAdapter);
+
+        HtmlParser htmlParser = new HtmlParser("http://www.fmi.pk.edu.pl/?page=rozklady_zajec.php&nc", arrayAdapter);
+        htmlParser.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
 }
--- app/src/main/java/pk/edu/pl/pk_wfmi_schedule_notificator/web/HtmlParser.java
+++ app/src/main/java/pk/edu/pl/pk_wfmi_schedule_notificator/web/HtmlParser.java
@@ -1,34 +1,65 @@
 package pk.edu.pl.pk_wfmi_schedule_notificator.web;
 
 
+import android.os.AsyncTask;
+import android.widget.ArrayAdapter;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
-public class HtmlParser {
-    private final static String url = "http://www.fmi.pk.edu.pl/?page=rozklady_zajec.php&nc";
+/**
+ * Looks for XML files on given page
+ */
+public class HtmlParser extends AsyncTask<Void, Void, String> {
+    private static final Logger logger = LoggerFactory.getLogger(HtmlParser.class);
 
-    /**
-     * Looks for XML files on given page
-     *
-     * @param pageUrl page address to download and parse a html
-     * @return list of found files
-     */
-    public String fetchXlsFiles(String pageUrl) throws IOException {
-        Document doc = Jsoup.connect(url).get();
-        Elements links = doc.select("a[href]");
-        for (Element link : links) {
-            String name = link.toString();
-            String name2;
-            if (name.indexOf("NIESTACJONARNE") != -1) {
-                name2 = name.substring(0, name.indexOf("xls")+3);
-                name2 = name2.substring(name2.lastIndexOf("/") + 1);
-                return name2;
+    private String url;
+    private ArrayAdapter<String> arrayAdapter;
+
+    public HtmlParser(String url, ArrayAdapter<String> arrayAdapter) {
+        this.url = url;
+        this.arrayAdapter = arrayAdapter;
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+        try {
+            logger.debug("Checking for new schedule");
+            Document doc = Jsoup.connect(url).timeout(1000 * 10).get();
+
+            Elements links = doc.select("a[href]");
+            for (Element link : links) {
+                String name = link.toString();
+                String name2;
+                if (name.indexOf("NIESTACJONARNE") != -1) {
+                    name2 = name.substring(0, name.indexOf("xls") + 3);
+                    name2 = name2.substring(name2.lastIndexOf("/") + 1);
+
+                    return name2;
+                }
             }
+
+        } catch (IOException e) {
+            logger.error("Network error",e);
         }
         return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        if (s != null) {
+            logger.debug("Overriding current schedule list with name: {}", s);
+
+            arrayAdapter.clear();
+            arrayAdapter.add(s);
+            arrayAdapter.notifyDataSetChanged();
+        }
     }
 }
