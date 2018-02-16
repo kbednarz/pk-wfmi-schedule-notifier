2980d5286fd27f2a74b56b2d2b9a1b8c150410df
kamex737 <kamex737@gmail.com>
2018-02-16 18:24:25 +0100

    Added about section

---
+++ app/src/main/java/pk/edu/pl/pk_wfmi_schedule_notificator/fragment/AboutFragment.java
@@ -0,0 +1,39 @@
+package pk.edu.pl.pk_wfmi_schedule_notificator.fragment;
+
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import pk.edu.pl.pk_wfmi_schedule_notificator.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AboutFragment extends Fragment {
+
+
+    public AboutFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        getActivity().getActionBar().setTitle(R.string.about_section);
+    }
+
+}
