6563c94e060cfa7a5a45149474c42271edbc2ad9
kamex737 <kamex737@gmail.com>
2018-02-16 19:05:20 +0100

    Filled about in content

--- app/src/main/java/pk/edu/pl/pk_wfmi_schedule_notificator/fragment/AboutFragment.java
+++ app/src/main/java/pk/edu/pl/pk_wfmi_schedule_notificator/fragment/AboutFragment.java
@@ -3,11 +3,13 @@ package pk.edu.pl.pk_wfmi_schedule_notificator.fragment;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import pk.edu.pl.pk_wfmi_schedule_notificator.R;
 
@@ -27,8 +29,25 @@ public class AboutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        View view = inflater.inflate(R.layout.fragment_about, container, false);
+
+        prepareGithubButton(view);
+
+        return view;
     }
+
+    private void prepareGithubButton(View view) {
+        Button githubButton = view.findViewById(R.id.github_button);
+        githubButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("https://github.com/kbednarz/pk-wfmi-schedule-notifier"));
+                startActivity(browserIntent);
+            }
+        });
+    }
+
 
     @Override
     public void onAttach(Activity activity) {
